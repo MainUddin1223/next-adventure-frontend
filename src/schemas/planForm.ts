5470197ff813fa9c0909223e8172198898243ab5
Md Main Uddin <2021md.main@gmail.com>
2023-11-27 16:08:09 +0600

    fixes

--- src/schemas/planForm.ts
+++ src/schemas/planForm.ts
@@ -1,17 +1,18 @@
 import * as yup from 'yup';
 
 export const planFormSchema = yup.object().shape({
-	plan_name: yup.string().required('Plan name is required'),
-	starting_location: yup.string().required('Starting location is required'),
+	planName: yup.string().required('Plan name is required'),
+	departureFrom: yup.string().required('Starting location is required'),
 	description: yup.string().required('Description is required'),
 	destination: yup.string().required('Destination is required'),
 	price: yup.number().required('Plan price is required'),
-	tour_duration: yup.number().required('Tour duration is required'),
-	total_meals: yup.number().required('Total meals is required'),
-	booking_deadline: yup.date().required('Booking deadline is required'),
-	starting_time: yup.date().required('Starting time is required'),
+	duration: yup.string().required('Tour duration is required'),
+	meals: yup.string().required('Total meals is required'),
+	deadline: yup.date().required('Booking deadline is required'),
+	departureTime: yup.date().required('Starting time is required'),
+	totalSeats: yup.string().required('Total seat is required'),
 	images: yup.array(yup.string().required()).required('Image is required'),
-	cover_location: yup
+	coverLocations: yup
 		.array(yup.string().required())
 		.min(1)
 		.required('Cover location is required'),
@@ -19,4 +20,8 @@ export const planFormSchema = yup.object().shape({
 		.array(yup.string().required())
 		.min(1)
 		.required('Events are required'),
+	notAllowed: yup
+		.array(yup.string().required())
+		.min(1)
+		.required('Not Allowed activities are required'),
 });
